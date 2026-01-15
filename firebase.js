44814219628be3710bb1f6ca1e74a9ea3408b72d
Zalyeee <joaomoreno789@gmail.com>
2026-01-15 02:48:54 -0300

    Atualizar o firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,24 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyAsHXQr5k3HviBVEFPz918g_zcjUZdFpJY",
-  authDomain: "cemiteryal-site.firebaseapp.com",
-  projectId: "cemiteryal-site",
-  storageBucket: "cemiteryal-site.firebasestorage.app",
-  messagingSenderId: "935123054222",
-  appId: "1:935123054222:web:1a047c9c051001ee94a3df"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyAsHXQr5k3HviBVEFPz918g_zcjUZdFpJY",
+    authDomain: "cemiteryal-site.firebaseapp.com",
+    projectId: "cemiteryal-site",
+    storageBucket: "cemiteryal-site.firebasestorage.app",
+    messagingSenderId: "935123054222",
+    appId: "1:935123054222:web:1a047c9c051001ee94a3df"
+  };
 
-export const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log("Logado:", user.email);
+    } else {
+      console.log("Não logado");
+    }
+  });
+</script>
