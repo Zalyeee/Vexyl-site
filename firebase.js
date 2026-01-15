ae542216d22cc2790505d56d6d6c86f8ed981e17
Zalyeee <joaomoreno789@gmail.com>
2026-01-15 02:33:02 -0300

    Add Firebase initialization and configuration

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAsHXQr5k3HviBVEFPz918g_zcjUZdFpJY",
+  authDomain: "cemiteryal-site.firebaseapp.com",
+  projectId: "cemiteryal-site",
+  storageBucket: "cemiteryal-site.firebasestorage.app",
+  messagingSenderId: "935123054222",
+  appId: "1:935123054222:web:1a047c9c051001ee94a3df"
+};
+
+export const app = initializeApp(firebaseConfig);
