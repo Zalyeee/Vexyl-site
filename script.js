46a95dc3fa13c4867c236f892f3bcb446d2b1369
Zalyeee <joaomoreno789@gmail.com>
2026-01-14 23:27:10 -0300

    Adjust particle generation based on screen width

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const container = document.querySelector(".particles")
 
-for (let i = 0; i < 150; i++) {
+const total = window.innerWidth < 600 ? 80 : 150
+
+for (let i = 0; i < total; i++) {
   const star = document.createElement("div")
   star.className = "particle"
 
-  const size = Math.random() * 2 + 1
+  const size = Math.random() * 2.5 + 1.5
   star.style.width = size + "px"
   star.style.height = size + "px"
 
   star.style.left = Math.random() * 100 + "vw"
   star.style.top = Math.random() * 100 + "vh"
 
+  star.style.opacity = Math.random() * 0.8 + 0.2
+
   star.style.animationDuration =
-    (Math.random() * 20 + 5) + "s"
+    (Math.random() * 20 + 10) + "s"
 
   container.appendChild(star)
 }
