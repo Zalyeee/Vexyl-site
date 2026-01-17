d87cf24e756409edff6193348988887e31e128d2
Zalyeee <joaomoreno789@gmail.com>
2026-01-17 00:34:54 -0300

    Atualizar o particles.js

--- particles.js
+++ particles.js
@@ -1,32 +1,36 @@
 const canvas = document.getElementById("particles")
 const ctx = canvas.getContext("2d")
-let w, h, stars = []
 
-function resize() {
-  w = canvas.width = window.innerWidth
-  h = canvas.height = window.innerHeight
-}
-window.onresize = resize
-resize()
+canvas.width = innerWidth
+canvas.height = innerHeight
 
-for (let i = 0; i < 120; i++) {
-  stars.push({
-    x: Math.random()*w,
-    y: Math.random()*h,
-    r: Math.random()*2
+let particles = []
+
+for (let i = 0; i < 60; i++) {
+  particles.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    r: Math.random() * 2 + 1,
+    vx: Math.random() - 0.5,
+    vy: Math.random() - 0.5
   })
 }
 
-function draw() {
-  ctx.clearRect(0,0,w,h)
-  ctx.fillStyle = "white"
-  stars.forEach(s=>{
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  particles.forEach(p => {
     ctx.beginPath()
-    ctx.arc(s.x,s.y,s.r,0,Math.PI*2)
+    ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2)
+    ctx.fillStyle = "white"
     ctx.fill()
-    s.y += .2
-    if (s.y > h) s.y = 0
+
+    p.x += p.vx
+    p.y += p.vy
+
+    if (p.x < 0 || p.x > canvas.width) p.vx *= -1
+    if (p.y < 0 || p.y > canvas.height) p.vy *= -1
   })
-  requestAnimationFrame(draw)
+  requestAnimationFrame(animate)
 }
-draw()
+
+animate()
