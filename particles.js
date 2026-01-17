3318d5a78a9995d266101c790e2a3af4b526975c
Zalyeee <joaomoreno789@gmail.com>
2026-01-17 12:37:17 -0300

    Atualizar o particles.js

--- particles.js
+++ particles.js
@@ -2,12 +2,7 @@ particlesJS("particles-js", {
   particles: {
     number: { value: 60 },
     color: { value: "#ffffff" },
-    shape: { type: "circle" },
-    opacity: { value: 0.6 },
     size: { value: 2 },
     move: { speed: 1 }
-  },
-  interactivity: {
-    events: { onhover: { enable: false } }
   }
 });
