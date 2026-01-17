43cb0902aa746bb7cc4ac06c8b0045d1dc9f034a
Zalyeee <joaomoreno789@gmail.com>
2026-01-17 13:43:53 -0300

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -1,62 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("menu-btn");
+  const menu = document.getElementById("side-menu");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAsHXQr5k3HviBVEFPz918g_zcjUZdFpJY",
-  authDomain: "cemiteryal-site.firebaseapp.com",
-  projectId: "cemiteryal-site",
-  storageBucket: "cemiteryal-site.firebasestorage.app",
-  messagingSenderId: "935123054222",
-  appId: "1:935123054222:web:f720f6ae514631dd94a3df"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// MENU
-const menuBtn = document.getElementById("menu-btn");
-const sideMenu = document.getElementById("side-menu");
-
-if (menuBtn) {
-  menuBtn.addEventListener("click", () => {
-    sideMenu.classList.toggle("active");
+  btn.addEventListener("click", () => {
+    menu.classList.toggle("active");
   });
-}
-
-// AUTH
-window.register = () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(() => window.location.href = "index.html")
-    .catch(alert);
-};
-
-window.login = () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => window.location.href = "index.html")
-    .catch(alert);
-};
-
-onAuthStateChanged(auth, user => {
-  const loginLink = document.getElementById("loginLink");
-  const registerLink = document.getElementById("registerLink");
-  const logoutBtn = document.getElementById("logoutBtn");
-
-  if (user) {
-    if (loginLink) loginLink.style.display = "none";
-    if (registerLink) registerLink.style.display = "none";
-    if (logoutBtn) {
-      logoutBtn.style.display = "block";
-      logoutBtn.onclick = () => signOut(auth);
-    }
-  }
 });
