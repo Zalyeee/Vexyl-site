5742e5bdc8414021540bc2cbfdab8b562b96a099
Zalyeee <joaomoreno789@gmail.com>
2026-01-18 03:39:03 -0300

    Excluir guard.js

--- js/guard.js
+++
@@ -1,9 +0,0 @@
-import { auth } from "./firebase.js";
-import { onAuthStateChanged } from
-"https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
-
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "login.html";
-  }
-});
