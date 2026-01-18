873b6c418b1a42ba77a999ccebdda9b7357484d5
Zalyeee <joaomoreno789@gmail.com>
2026-01-18 03:46:30 -0300

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuBtn = document.getElementById("menuBtn");
-  const sideMenu = document.getElementById("sideMenu");
-  const overlay = document.getElementById("overlay");
+import { auth } from "./firebase.js";
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-  menuBtn.addEventListener("click", () => {
-    sideMenu.classList.add("active");
-    overlay.classList.add("active");
-  });
+const menuBtn = document.getElementById("menu-btn");
+const sideMenu = document.getElementById("side-menu");
 
-  overlay.addEventListener("click", () => {
-    sideMenu.classList.remove("active");
-    overlay.classList.remove("active");
-  });
+menuBtn.onclick = () => {
+  sideMenu.classList.toggle("show");
+};
+
+const loginLink = document.getElementById("loginLink");
+const registerLink = document.getElementById("registerLink");
+const logoutBtn = document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth, user => {
+  if (user) {
+    loginLink.style.display = "none";
+    registerLink.style.display = "none";
+    logoutBtn.style.display = "block";
+  } else {
+    loginLink.style.display = "block";
+    registerLink.style.display = "block";
+    logoutBtn.style.display = "none";
+  }
 });
